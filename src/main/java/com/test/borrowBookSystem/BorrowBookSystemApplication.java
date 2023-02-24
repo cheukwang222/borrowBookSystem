1a0cfbcd39841eb0bb956d57ab2ab27dd8ded551
Terry Cheung <terry.cheung@bbmsl.com>
2023-02-24 12:39:06 +0800

    init

---
+++ src/main/java/com/test/borrowBookSystem/BorrowBookSystemApplication.java
@@ -0,0 +1,13 @@
+package com.test.borrowBookSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BorrowBookSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BorrowBookSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/borrowBookSystem/BorrowBookSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.borrowBookSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BorrowBookSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
